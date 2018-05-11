f1fe845de57b771e91c51502c7a52c254e563989
Smeurfy <dennydenii@gmail.com>
2018-05-11 20:33:16 +0100

    as cenas que fizemos

---
+++ bin/startservers.bat
@@ -0,0 +1,9 @@
+cd../tax & start cmd /k mvn clean spring-boot:run
+cd../bank & start cmd /k mvn clean spring-boot:run 
+cd../hotel & start cmd /k mvn clean spring-boot:run 
+cd../activity & start cmd /k mvn clean spring-boot:run 
+cd../car & start cmd /k mvn clean spring-boot:run 
+cd../broker & start cmd /k mvn clean spring-boot:run 
+cd../bin
+
+
